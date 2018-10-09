716ac58ce33919d2ee4706920c58b74e673eb4d4
Freddie Molina <freddiejmo@gmail.com>
2018-10-09 18:15:40 -0400

    create JEE-7 folder

---
+++ JEE7/Readme.java
@@ -0,0 +1,11 @@
+Creating JEE project in Netbeans
+1- click on "File"
+2- click on "new project" 
+3- select "Java Web"
+3- select "Web Application"
+4- click on "next" button
+5- edit "Project Name"
+6- select "Project Location"
+7- click on "next" button
+8- clik on "finish" button
+
