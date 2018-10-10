b85c5db5dc71d4d3ef43a0696f085122adace242
Freddie Molina <freddiejmo@gmail.com>
2018-10-10 06:21:42 -0400

    changes in requirements

--- JEE7/requirements.java
+++ JEE7/requirements.java
@@ -1,5 +1,26 @@
+Install
 1- IDE (Netbeans)
 2- JEE
 3- GlassFish
 4- Mysql
+
+Create database "rrhh"
+
+adde "person" table with:
+	person_id: pk
+	name: varchar(45)
+	last_name: varchar(45)
+	email: varchar(45)
+	phone: varchar(45) unique
+
+Create database connection on netbeans
+1- click on "services" tab
+2- right click on "Databases"
+3- click on "New Connection"
+4- Select in Driver "MySQL (Connector/J driver)"
+5- click on "next" button
+6- edit in "Database:" input, set "rrrhh"
+7- edit "User Name:" input, set "usernameMySQLConfigured"
+8- edit "Password:" input, set "passwordMySQLConfigured"
+9- click on "Test Connection" button
 
